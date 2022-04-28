a08ee77b0635acb1858418ab208c3e6dc32c64df
Daniel Duberg <danielduberg@gmail.com>
2022-04-28 15:37:14 +0200

    Removed unused messages

--- ufomap_ros/ufomap_msgs/include/ufomap_msgs/conversions.h
+++ ufomap_ros/ufomap_msgs/include/ufomap_msgs/conversions.h
@@ -41,83 +41,15 @@
 #ifndef UFOMAP_ROS_MSGS_CONVERSIONS_H
 #define UFOMAP_ROS_MSGS_CONVERSIONS_H
 
-// UFO
-#include <ufo/geometry/aabb.h>
-#include <ufo/geometry/bounding_volume.h>
-#include <ufo/geometry/frustum.h>
-#include <ufo/geometry/line_segment.h>
-#include <ufo/geometry/obb.h>
-#include <ufo/geometry/plane.h>
-#include <ufo/geometry/point.h>
-#include <ufo/geometry/ray.h>
-#include <ufo/geometry/sphere.h>
-#include <ufo/geometry/types.h>
-#include <ufo/map/io.h>
-#include <ufo/map/predicate/predicates.h>
-#include <ufo/map/types.h>
-
-// UFO msg
-#include <ufomap_msgs/AABB.h>
-#include <ufomap_msgs/BoundingVolume.h>
-#include <ufomap_msgs/Frustum.h>
-#include <ufomap_msgs/LineSegment.h>
-#include <ufomap_msgs/OBB.h>
-#include <ufomap_msgs/Plane.h>
-#include <ufomap_msgs/Point.h>
-#include <ufomap_msgs/Ray.h>
-#include <ufomap_msgs/Sphere.h>
+// UFO ROS
 #include <ufomap_msgs/UFOMap.h>
 
 // STL
-#include <optional>
+#include <sstream>
 #include <type_traits>
 
 namespace ufomap_msgs
 {
-//
-// ROS message type to UFO type
-//
-
-ufo::geometry::Point msgToUfo(ufomap_msgs::Point const& point);
-
-ufo::geometry::AABB msgToUfo(ufomap_msgs::AABB const& aabb);
-
-ufo::geometry::Plane msgToUfo(ufomap_msgs::Plane const& plane);
-
-ufo::geometry::Frustum msgToUfo(ufomap_msgs::Frustum const& frustum);
-
-ufo::geometry::LineSegment msgToUfo(ufomap_msgs::LineSegment const& line_segment);
-
-ufo::geometry::OBB msgToUfo(ufomap_msgs::OBB const& obb);
-
-ufo::geometry::Ray msgToUfo(ufomap_msgs::Ray const& ray);
-
-ufo::geometry::Sphere msgToUfo(ufomap_msgs::Sphere const& sphere);
-
-ufo::geometry::BoundingVolume msgToUfo(ufomap_msgs::BoundingVolume const& msg);
-
-//
-// UFO type to ROS message type
-//
-
-ufomap_msgs::Point ufoToMsg(ufo::geometry::Point const& point);
-
-ufomap_msgs::AABB ufoToMsg(ufo::geometry::AABB const& aabb);
-
-ufomap_msgs::Plane ufoToMsg(ufo::geometry::Plane const& plane);
-
-ufomap_msgs::Frustum ufoToMsg(ufo::geometry::Frustum const& frustum);
-
-ufomap_msgs::LineSegment ufoToMsg(ufo::geometry::LineSegment const& line_segment);
-
-ufomap_msgs::OBB ufoToMsg(ufo::geometry::OBB const& obb);
-
-ufomap_msgs::Ray ufoToMsg(ufo::geometry::Ray const& ray);
-
-ufomap_msgs::Sphere ufoToMsg(ufo::geometry::Sphere const& sphere);
-
-ufomap_msgs::BoundingVolume ufoToMsg(
-    ufo::geometry::BoundingVolume const& bounding_volume);
 
 //
 // ROS message type to UFO type
@@ -146,7 +78,7 @@ void msgToUfo(ufomap_msgs::UFOMap const& msg, Map& map, bool propagate = true)
 template <class Map, class Predicates,
           typename = std::enable_if_t<!std::is_scalar_v<Predicates>>>
 ufomap_msgs::UFOMap ufoToMsg(Map const& map, Predicates const& predicates,
-                             ufo::map::DepthType depth = 0, bool compress = false,
+                             unsigned int depth = 0, bool compress = false,
                              int compression_acceleration_level = 1,
                              int compression_level = 0)
 {
@@ -164,13 +96,21 @@ ufomap_msgs::UFOMap ufoToMsg(Map const& map, Predicates const& predicates,
 }
 
 template <class Map>
-ufomap_msgs::UFOMap ufoToMsg(Map const& map, ufo::map::DepthType depth = 0,
+ufomap_msgs::UFOMap ufoToMsg(Map const& map, unsigned int depth = 0,
                              bool compress = false,
                              int compression_acceleration_level = 1,
                              int compression_level = 0)
 {
-	return ufoToMsg(map, ufo::map::predicate::TRUE(), depth, compress,
-	                compression_acceleration_level, compression_level);
+	std::stringstream data(std::ios_base::in | std::ios_base::out | std::ios_base::binary);
+	data.exceptions(std::stringstream::failbit | std::stringstream::badbit);
+
+	map.write(data, depth, compress, compression_acceleration_level, compression_level);
+
+	ufomap_msgs::UFOMap msg;
+	msg.data.resize(data.tellp());
+	data.seekg(0, std::ios_base::beg);
+	data.read(reinterpret_cast<char*>(msg.data.data()), msg.data.size());
+	return msg;
 }
 
 }  // namespace ufomap_msgs
