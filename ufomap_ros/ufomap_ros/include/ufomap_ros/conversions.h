469f12a5d55db9cb6fab814bbebcdaf038d4f58e
Daniel Duberg <danielduberg@gmail.com>
2022-05-02 09:15:02 +0200

    Made constexpr

--- ufomap_ros/ufomap_ros/include/ufomap_ros/conversions.h
+++ ufomap_ros/ufomap_ros/include/ufomap_ros/conversions.h
@@ -50,10 +50,10 @@
 
 // ROS
 #include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Transform.h>
 #include <geometry_msgs/Vector3.h>
-#include <sensor_msgs/Image.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointField.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
@@ -244,41 +244,147 @@ void ufoToRos(ufo::map::PointCloudT<T> const& cloud_in,
 }
 
 // Vector3/Point
-void rosToUfo(geometry_msgs::Point const& point_in, ufo::math::Vector3& point_out);
+template <typename T>
+constexpr void rosToUfo(geometry_msgs::Point const& point_in,
+                        ufo::math::Vector3<T>& point_out)
+{
+	point_out.x = point_in.x;
+	point_out.y = point_in.y;
+	point_out.z = point_in.z;
+}
 
-void rosToUfo(geometry_msgs::Vector3 const& point_in, ufo::math::Vector3& point_out);
+template <typename T>
+constexpr void rosToUfo(geometry_msgs::Vector3 const& point_in,
+                        ufo::math::Vector3<T>& point_out)
+{
+	point_out.x = point_in.x;
+	point_out.y = point_in.y;
+	point_out.z = point_in.z;
+}
 
-ufo::math::Vector3 rosToUfo(geometry_msgs::Point const& point);
+constexpr ufo::math::Vector3d rosToUfo(geometry_msgs::Point const& point)
+{
+	return ufo::math::Vector3d(point.x, point.y, point.z);
+}
 
-ufo::math::Vector3 rosToUfo(geometry_msgs::Vector3 const& point);
+constexpr ufo::math::Vector3d rosToUfo(geometry_msgs::Vector3 const& point)
+{
+	return ufo::math::Vector3d(point.x, point.y, point.z);
+}
 
-void ufoToRos(ufo::math::Vector3 const& point_in, geometry_msgs::Point& point_out);
+template <typename T>
+constexpr void ufoToRos(ufo::math::Vector3<T> const& point_in,
+                        geometry_msgs::Point& point_out)
+{
+	point_out.x = point_in.x;
+	point_out.y = point_in.y;
+	point_out.z = point_in.z;
+}
 
-void ufoToRos(ufo::math::Vector3 const& point_in, geometry_msgs::Vector3& point_out);
+template <typename T>
+constexpr void ufoToRos(ufo::math::Vector3<T> const& point_in,
+                        geometry_msgs::Vector3& point_out)
+{
+	point_out.x = point_in.x();
+	point_out.y = point_in.y();
+	point_out.z = point_in.z();
+}
 
-geometry_msgs::Point ufoToRos(ufo::math::Vector3 const& point);
+template <typename P, typename T>
+constexpr P ufoToRos(ufo::math::Vector3<T> const& point)
+{
+	P p;
+	ufoToRos(point, p);
+	return p;
+}
 
 // Quaternion
-void rosToUfo(geometry_msgs::Quaternion const& quaternion_in,
-              ufo::math::Quaternion& quaternion_out);
+template <typename T>
+constexpr void rosToUfo(geometry_msgs::Quaternion const& quaternion_in,
+                        ufo::math::Quaternion<T>& quaternion_out)
+{
+	quaternion_out.x() = quaternion_in.x;
+	quaternion_out.y() = quaternion_in.y;
+	quaternion_out.z() = quaternion_in.z;
+	quaternion_out.w() = quaternion_in.w;
+}
 
-ufo::math::Quaternion rosToUfo(const geometry_msgs::Quaternion& quaternion);
+constexpr ufo::math::Quaterniond rosToUfo(geometry_msgs::Quaternion const& quaternion)
+{
+	return ufo::math::Quaterniond(quaternion.w, quaternion.x, quaternion.y, quaternion.z);
+}
 
-void ufoToRos(ufo::math::Quaternion const& quaternion_in,
-              geometry_msgs::Quaternion& quaternion_out);
+template <typename T>
+constexpr void ufoToRos(ufo::math::Quaternion<T> const& quaternion_in,
+                        geometry_msgs::Quaternion& quaternion_out)
+{
+	quaternion_out.x = quaternion_in.x();
+	quaternion_out.y = quaternion_in.y();
+	quaternion_out.z = quaternion_in.z();
+	quaternion_out.w = quaternion_in.w();
+}
 
-geometry_msgs::Quaternion ufoToRos(ufo::math::Quaternion const& quaternion);
+template <typename T>
+constexpr geometry_msgs::Quaternion ufoToRos(ufo::math::Quaternion<T> const& quaternion)
+{
+	geometry_msgs::Quaternion q;
+	ufoToRos(quaternion, q);
+	return q;
+}
 
-// Transforms
-void rosToUfo(geometry_msgs::Transform const& transform_in,
-              ufo::math::Pose6& transform_out);
+// Transforms/Pose
+template <typename T>
+constexpr void rosToUfo(geometry_msgs::Transform const& transform_in,
+                        ufo::math::Pose6<T>& transform_out)
+{
+	rosToUfo(transform_in.translation, transform_out.translation());
+	rosToUfo(transform_in.rotation, transform_out.rotation());
+}
 
-ufo::math::Pose6 rosToUfo(geometry_msgs::Transform const& transform);
+template <typename T>
+constexpr void rosToUfo(geometry_msgs::Pose const& pose_in, ufo::math::Pose6<T>& pose_out)
+{
+	rosToUfo(pose_in.position, pose_out.translation());
+	rosToUfo(pose_in.orientation, pose_out.rotation());
+}
 
-void ufoToRos(ufo::math::Pose6 const& transform_in,
-              geometry_msgs::Transform& transform_out);
+constexpr ufo::math::Pose6d rosToUfo(geometry_msgs::Transform const& transform)
+{
+	return ufo::math::Pose6d(transform.translation.x, transform.translation.y,
+	                         transform.translation.z, transform.rotation.w,
+	                         transform.rotation.x, transform.rotation.y,
+	                         transform.rotation.z);
+}
 
-geometry_msgs::Transform ufoToRos(ufo::math::Pose6 const& transform);
+constexpr ufo::math::Pose6d rosToUfo(geometry_msgs::Pose const& pose)
+{
+	return ufo::math::Pose6d(pose.position.x, pose.position.y, pose.position.z,
+	                         pose.orientation.w, pose.orientation.x, pose.orientation.y,
+	                         pose.orientation.z);
+}
+
+template <typename T>
+constexpr void ufoToRos(ufo::math::Pose6<T> const& transform_in,
+                        geometry_msgs::Transform& transform_out)
+{
+	ufoToRos(transform_in.translation(), transform_out.translation);
+	ufoToRos(transform_in.rotation(), transform_out.rotation);
+}
+
+template <typename T>
+constexpr void ufoToRos(ufo::math::Pose6<T> const& pose_in, geometry_msgs::Pose& pose_out)
+{
+	ufoToRos(pose_in.translation(), pose_out.position);
+	ufoToRos(pose_in.rotation(), pose_out.orientation);
+}
+
+template <typename G, typename T>
+constexpr G ufoToRos(ufo::math::Pose6<T> const& transform)
+{
+	G g;
+	ufoToRos(transform, g);
+	return g;
+}
 }  // namespace ufomap_ros
 
 #endif  // UFOMAP_ROS_CONVERSIONS_H
