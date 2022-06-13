154a56ca3722ea56f242f3a5289f5dfd3d88a9a5
Daniel Duberg <danielduberg@gmail.com>
2022-06-13 12:20:51 +0200

    Updated header

--- ufomap/include/ufo/map/occupancy_map_time_color_semantic.h
+++ ufomap/include/ufo/map/occupancy_map_time_color_semantic.h
@@ -1,31 +1,31 @@
 /*!
  * UFOMap: An Efficient Probabilistic 3D Mapping Framework That Embraces the Unknown
  *
- * @author D. Duberg, KTH Royal Institute of Technology, Copyright (c) 2020.
+ * @author Daniel Duberg (dduberg@kth.se)
  * @see https://github.com/UnknownFreeOccupied/ufomap
- * License: BSD 3
+ * @version 1.0
+ * @date 2022-05-13
  *
- */
-
-/*
+ * @copyright Copyright (c) 2022, Daniel Duberg, KTH Royal Institute of Technology
+ *
  * BSD 3-Clause License
  *
- * Copyright (c) 2020, D. Duberg, KTH Royal Institute of Technology
+ * Copyright (c) 2022, Daniel Duberg, KTH Royal Institute of Technology
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ *     list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
  *
  * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
